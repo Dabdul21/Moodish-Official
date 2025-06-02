b577aca289d8eff5c83ad3bffc35639162c2149c
Otis YII <oyoung21@gmail.com>
2025-06-02 06:35:36 -0400

    Added a base Mood Chllenge View hope to have it change with the selected mood from mood view by the end of the day

--- LateNitesEarlyMornin Watch App/Views/ChallengeView.swift
+++ LateNitesEarlyMornin Watch App/Views/ChallengeView.swift
@@ -5,14 +5,64 @@
 //  Created by Dayan Abdulla on 5/27/25.
 //
 
+
+    
+    
 import SwiftUI
 
 struct ChallengeView: View {
+    let mood: Mood
+    @State private var currentChallengeIndex = 0
+
     var body: some View {
-        Text("Challenge View Screen!")
+        VStack(spacing: 20) {
+            Text("Mood: \(mood.label)")
+                .font(.headline)
+                .foregroundColor(mood.color)
+
+            Text(mood.challenges[currentChallengeIndex])
+                .font(.title3)
+                .padding()
+
+            Button("Complete Challenge") {
+                if currentChallengeIndex < mood.challenges.count - 1 {
+                    currentChallengeIndex += 1
+                } else {
+                    currentChallengeIndex = 0 // Reset to first challenge
+                }
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+
+    
+extension Mood {
+    var challenges: [String] {
+        switch self {
+        case .overwhelmed:
+            return ["Take deep breaths", "Do a quick stretch", "Write out your thoughts"]
+        case .happy:
+            return ["Share your happiness", "Dance for 1 minute", "Write down what made you happy"]
+        case .excited:
+            return ["Channel your energy", "Plan your day", "Talk to a friend"]
+        case .calm:
+            return ["Close your eyes for 1 min", "Listen to calm music", "Do deep breathing"]
+        case .angry:
+            return ["Take deep breaths", "Write your thoughts", "Go for a walk"]
+        case .nervous:
+            return ["Play a calming sound", "Take deep breaths", "Count backward from 30"]
+        case .sad:
+            return ["Watch something funny", "Talk to someone", "Write how you feel"]
+        case .tired:
+            return ["Stretch for 1 min", "Drink water", "Take a short break"]
+        }
     }
 }
 
 #Preview {
-    ChallengeView()
+    // Preview using a single mood (e.g., .nervous)
+    ChallengeView(mood: .nervous)
 }
